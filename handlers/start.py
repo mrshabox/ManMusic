950ec0d25d58af767c0dd46f9f41e6ce10918aa0
mrshabox <106615216+mrshabox@users.noreply.github.com>
2022-06-17 22:00:16 +0700

    Update start.py

--- handlers/start.py
+++ handlers/start.py
@@ -13,41 +13,18 @@ async def start_(client: Client, message: Message):
         photo=f"{START_IMG}",
         caption=f"""👋 Hai {message.from_user.mention()} 
 
-Saya adalah [{bn}](t.me/{bu}), bot yang bisa streaming musik di OS/VCG !
-jangan sungkan tambahkan saya ke grup kalian ya...
-
-Jika kalian punya pertanyaan atau masalah lainnya?
-kalian bisa hubungi ➥ [Owner](t.me/erojistrix)
-
-**Kalian ingin berdonasi? boleh pake bangettt**
-
-[Donasi](https://bit.ly/donasidhimasazman) - [Join Channel](t.me/AzumanProjects) - [Command List](https://telegra.ph/COMMAND-LIST-06-10)
-
-Ingin Menambahkan Saya ke Grup Anda? Cukup Klik Tombol di Bawah""",
+Tôi là [{bn}](t.me/{bu}), bot có thể phát nhạc trên OS / VCG!
+ đừng ngần ngại thêm tôi vào nhóm của bạn ...""",
     reply_markup=InlineKeyboardMarkup(
             [
                 [
                     InlineKeyboardButton(
-                    "➕ ᴛᴀᴍʙᴀʜᴋᴀɴ ꜱᴀʏᴀ ᴋᴇ ɢʀᴜᴘ ᴀɴᴅᴀ", url=f"https://t.me/{bu}?startgroup=true"
+                    "➕ Thêm vào nhóm", url=f"https://t.me/{bu}?startgroup=true"
                     ),
                 ],[
                     InlineKeyboardButton(
-                        "👤 ᴅᴇᴠᴇʟᴏᴘᴇʀ", url=f"https://t.me/erojistrix"
-                    ),
-                    InlineKeyboardButton(
-                        "📥 ᴏꜰꜰɪᴄɪᴀʟ ɢʀᴏᴜᴘ", url=f"https://t.me/{SUPPORT_GROUP}"
+                        "➥ Inline", switch_inline_query_current_chat=""
                     )
-                ],[
-                    InlineKeyboardButton(
-                        "➥ ɪɴʟɪɴᴇ", switch_inline_query_current_chat=""
-                    ),
-                    InlineKeyboardButton(
-                        "📺 ʏᴏᴜᴛᴜʙᴇ", url="https://youtube.com/c/dhimasazman"
-                    )
-                ],[
-                    InlineKeyboardButton(
-                        "📝 ʀᴇᴘᴏꜱɪᴛᴏʀʏ", url=f"https://github.com/dhimasazman/ManMusic"
-                    ),
                 ]
             ]
        ),
